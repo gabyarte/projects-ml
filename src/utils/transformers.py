2f340b03817862dfc6b8202d3e1b4ab36e65dae8
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2022-11-17 23:08:34 +0100

    ref: Refactor transformers code

--- src/utils/transformers.py
+++ src/utils/transformers.py
@@ -17,10 +17,7 @@ class AssignTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        if self.copy:
-            X_ = X.copy()
-        else:
-            X_ = X
+        X_ = X.copy() if self.copy else X
         return X_.assign(**self.assign_map)
 
 class NameTransformer(BaseEstimator, TransformerMixin):
@@ -35,7 +32,6 @@ class NameTransformer(BaseEstimator, TransformerMixin):
                 f"""The following columns are defined both in `keep_features`
                 and in `names_map.keys()`:\n{intersection_}
                 """
-
         self.names_map = names_map
         self.keep_features = keep_features
         self.copy = copy
@@ -44,22 +40,20 @@ class NameTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
+        def copy_if(df, condition):
+            return df.copy() if condition else df
+
         # if `keep_features` is list, keep only renamed and listed columns
         if isinstance(self.keep_features, list):
-            if self.copy:
-                X_ = X[list(self.names_map.keys()) + self.keep_features].copy()
-            else:
-                X_ = X[list(self.names_map.keys()) + self.keep_features]
+            X_ = copy_if(X[list(self.names_map.keys()) + self.keep_features],
+                         self.copy)
         elif isinstance(self.keep_features, bool):
             # if `keep_features` is False, keep only renamed variables
             if not self.keep_features:
-                if self.copy:
-                    X_ = X[list(self.names_map.keys())].copy()
-                else:
-                    X_ = X[list(self.names_map.keys())]
+                    X_ = copy_if(X[list(self.names_map.keys())], self.copy)
             # if `keep_features` is True, keep all variables
             else:
-                X_ = X.copy() if self.copy else X
+                X_ = copy_if(X, self.copy)
         else:
             X_ = None 
         X_ = X_.rename(self.names_map, axis=1)
@@ -71,10 +65,10 @@ class MergeTransformer(BaseEstimator, TransformerMixin):
     """
 
     def __init__(
-            self, get_right_df, keep_index=True, merge_kwargs=None, copy=True):
+            self, right_df, keep_index=True, merge_kwargs=None, copy=True):
         if merge_kwargs is None:
             merge_kwargs = {}
-        self.get_right_df = get_right_df
+        self.right_df = right_df
         self.keep_index = keep_index
         self.merge_kwargs = merge_kwargs.copy()
         self.copy = copy
@@ -84,7 +78,6 @@ class MergeTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, df):
         df_ = df.copy() if self.copy else df
-        right_df = self.get_right_df()
         if self.keep_index:
             index_names = df_.index.names
             # handle unnamed index
@@ -94,38 +87,39 @@ class MergeTransformer(BaseEstimator, TransformerMixin):
             )
             index_names = ['index'] if is_unnamed_index else index_names
             # merge respecting index and `merge_kwargs`
-            df_ = df_.reset_index().merge(right_df, **self.merge_kwargs
-                                          ).set_index(index_names)
+            df_ = df_.reset_index() \
+                     .merge(self.right_df, **self.merge_kwargs) \
+                     .set_index(index_names)
             if is_unnamed_index:
                 df_.index.names = [None]
         else:
-            df_ = df_.merge(right_df, **self.merge_kwargs)
+            df_ = df_.merge(self.right_df, **self.merge_kwargs)
         return df_
 
 
 class OneHotPandas(OneHotEncoder):
-    def __init__( self, 
-                  categories = 'auto', drop=None, sparse = False, dtype = np.int, 
-                  handle_unknown = 'ignore', key_var = None ):
+    def __init__(self, categories='auto', drop=None, sparse=False,
+                 dtype=np.int, handle_unknown='ignore', key_var=None ):
         # OneHotEncoder originals
-        super().__init__(categories=categories, drop=drop, 
-                         sparse=sparse, dtype=dtype, handle_unknown=handle_unknown)
-                
+        super().__init__(categories=categories, drop=drop, sparse=sparse,
+                         dtype=dtype, handle_unknown=handle_unknown)
+
         # In order to keep pandas column names
         self.original_names = None
         self.feature_names = None
         self.key_var = key_var
 
     def clean_feature_names(self, cols):
-        new_names=[]
+        new_names = []
         for col in cols:
             new_names.append(col.replace(".0", ""))
         return new_names
 
     def fit(self, X, y=None):
-        _fit = super().fit(X.set_index(self.key_var))
+        _X = X.set_index(self.key_var)
+        _fit = super().fit(_X)
         self.original_names = self.clean_feature_names(
-            _fit.get_feature_names(list(X.set_index(self.key_var).columns)))
+            _fit.get_feature_names(list(_X.columns)))
         return _fit
 
     def transform(self, X, y=None):
@@ -135,4 +129,6 @@ class OneHotPandas(OneHotEncoder):
             columns=self.original_names)
         self.feature_names= [f for f in self.original_names if not ('9999' in f or '99' in f)]
 
-        return pd.concat([X[[self.key_var]],_transform[ self.feature_names]], axis=1)
+        return pd.concat(
+            [X[[self.key_var]], _transform[self.feature_names]],
+            axis=1)
