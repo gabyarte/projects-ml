2da36bb97e431f5c8053e621a71c69bde44f4afb
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2023-01-02 17:14:28 +0100

    feat: Improve score plots and generate selected dataset

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def plot_features_scores(
-    X, scores, metric_label=r'$-log(p_{value})$'):
+    X, scores, method='univariate', metric_label=r'$-log(p_{value})$'):
     scores_df = pd.DataFrame({'scores': scores, 'columns': X.columns}) \
-    .sort_values('scores', ascending=False)
+        .sort_values('scores', ascending=False)
     plt.figure(figsize=(10, 20))
     sns.barplot(data=scores_df, x='scores', y='columns', color='blue')
     plt.grid(True)
-    plt.title("Feature univariate score")
-    plt.xlabel(f'Unvariate score ({metric_label})')
+    plt.title(f'Feature {method} score')
+    plt.xlabel(f'{method.capitalize()} score ({metric_label})')
     plt.ylabel('')
     plt.show()
