7548b3accada8889f77f5160799db080d31de3a1
Gabriela Argüelles Terrón <gabyarte2@gmail.com>
2023-06-15 19:35:33 +0200

    feat: Add SVM model results and decision boundary

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.decomposition import PCA
+
+
 def plot_features_scores(
     X, scores, method='univariate', metric_label=r'$-log(p_{value})$'):
     scores_df = pd.DataFrame({'scores': scores, 'columns': X.columns}) \
@@ -12,4 +16,27 @@ def plot_features_scores(
     plt.title(f'Feature {method} score')
     plt.xlabel(f'{method.capitalize()} score ({metric_label})')
     plt.ylabel('')
+    plt.show()
+
+
+def decision_boundary_plot(estimator, X, y, model='model'):
+    _X = PCA(n_components=2).fit_transform(X, y)
+    _X = pd.DataFrame(_X)
+
+    decision_boundary = DecisionBoundaryDisplay.from_estimator(
+        estimator.fit(_X, y['y']),
+        _X,
+        response_method='predict',
+        cmap=plt.cm.coolwarm,
+        alpha=0.8
+    )
+    decision_boundary.ax_.scatter(
+        x=_X[0],
+        y=_X[1],
+        c=y['y'],
+        cmap=plt.cm.coolwarm,
+        s=20,
+        edgecolors='k'
+    )
+    plt.title(f'Decision boundary of {model}')
     plt.show()
